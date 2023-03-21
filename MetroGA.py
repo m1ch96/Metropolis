f6c251f9f479f0829d928d9e78bea4ea61ef0527
micheleproietto <micheleproietto96@gmail.com>
2023-03-21 17:39:55 +0100

    final changes in code and add input.json file

--- MetroGA.py
+++ MetroGA.py
@@ -3,9 +3,8 @@ from mpl_toolkits import mplot3d
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy.linalg import lstsq
-import os
-import sys
 import csv
+import json
 
 
 #ststs
@@ -41,13 +40,14 @@ def Sigmap(x,covd,covm,mud,mum,t,hoP,hoC,hoSc, yP, yC, ySc):
             gmshc.append(abs((1 - np.exp(-x[0] * t[i])) * (hC - x[1] * hoC + yC[i] * (x[1] -1))))
             gmshSc.append(((1 - np.exp(-x[2] * t[i])) * (hSc -x[3] * hoSc + ySc[i] * (x[3] -1))))     
             gm.append(gmox[i] + gmshc[i] + gmshSc[i])
-         
+
+ 
     msfit = 0.5 * (np.matmul((np.matmul((gm - mud),np.linalg.inv(covD))), (gm - mud).T) + np.matmul((np.matmul((x - mum).T, np.linalg.inv(covm))), (x - mum)))
     return np.exp(-msfit)
     
     
 
-def metropolis3(N3, num_steps3, delta3,mum,covm,mud,covd,x,t,hoP,hoC,hoSc, yP, yC, ySc):
+def metropolis(N3, num_steps3, delta3,mum,covm,mud,covd,x,t,hoP,hoC,hoSc, yP, yC, ySc):
     samples3 = [ ]        
     for i in range(num_steps3):
         # Propose a new point
@@ -68,11 +68,9 @@ def metropolis3(N3, num_steps3, delta3,mum,covm,mud,covd,x,t,hoP,hoC,hoSc, yP, y
         Prob3[i] = Sigmap(samples3[i],covd,covm,mud,mum,t,hoP,hoC,hoSc, yP, yC, ySc)
            
     acceptance_rate3 = len(samples3) / num_steps3
-    print("Acceptance rate is:")
-    print(acceptance_rate3)
-    print("The mumber of the samples are")
-    print(len(samples3))
-
+    print("Acceptance rate is: ", acceptance_rate3) 
+    print("The mumber of the samples are ", len(samples3))
+  
     return samples3,Prob3
 
 
@@ -117,7 +115,7 @@ def FindNewStartingPoint(Prob3,samples3):
         if Prob3[i] == MaxProb3:
            IndexMaxProb = i
            break       
-    print("Nuovo punto di partenza:",samples3[IndexMaxProb])
+    print("Nuovo punto di partenza:", samples3[IndexMaxProb])
 
     return samples3[IndexMaxProb]
 
@@ -175,7 +173,92 @@ def Plot(gm,t,yP,yC,ySc,mud,devstd,samples3,Prob3):
     plt.title("Prability of the functions on each samples for data")
     plt.plot([i for i in range(len(samples3))], Prob3)
 
-    plt.show()  
+    plt.show()
+
+def CovarianceMatrix(mum,devstm,Ncm,Ncd,mud,devstd): 
+    samplesm= np.zeros((Ncm,len(mum)))
+    for i in range(len(mum)):
+        samplesm[:,i] = np.random.normal(mum[i], devstm[i], Ncm)
+    covm = np.cov(samplesm.T)
+
+    samplesd= np.zeros((Ncd,len(mud)))
+    for i in range(len(mud)):    
+        samplesd[:,i] = np.random.normal(mud[i], devstd[i], Ncd)
+    covd = np.cov(samplesd.T)
+    
+    return covd,covm
+
+
+def Groundwaterlevel(t,hwSc,hwP,hwC,Choose,hoP,hoSc,hoC):
+    
+    tfit =np.array([t[0],t[(len(t)-1)]])
+
+    #Parameters of straight line
+    B = np.stack([tfit, np.ones(len(tfit))]).T
+    mSc, cSc = np.linalg.lstsq(B, hwSc, rcond=None)[0]
+    C = np.stack([tfit, np.ones(len(tfit))]).T
+    mP, cP = np.linalg.lstsq(C, hwP, rcond=None)[0]   
+    A = np.stack([tfit, np.ones(len(tfit))]).T
+    mC, cC = np.linalg.lstsq(A, hwC, rcond=None)[0]
+       
+    if Choose == "Linear":
+       #Linear trend for hwet:
+       #h wet clay
+       yC = mC*t + cC
+       #h wet Sandy clay
+       ySc = mSc*t + cSc
+       #h wet Peat
+       yP = mP*t + cP
+
+    elif Choose == "Sin":
+        #Sin trend for hwet:
+        #h wet Sandy clay
+        if hoSc != hwSc[1]:
+           ySc=mSc * t +cSc + 0.2 * np.sin(t-np.pi)
+        else:
+            ySc=mSc * t +cSc
+        #h wet clay
+        if hoC != hwC[1]:
+           yC = mC*t + cC + 0.2 * np.sin(t-np.pi)
+        else:
+            yC = mC*t + cC 
+        #h wet Peat
+        if hoP != hwP[1]:
+           yP = mP*t + cP + 0.2 * np.sin(t-np.pi)
+        else:
+            yP = mP*t + cP
+    
+    return yC,yP,ySc
+
+def ExtractData():
+    
+    # load the JSON data from file
+    with open('Input.json', 'r') as f:
+         data = json.load(f)
+
+    # extract the value of a variable from the JSON data
+    t = data['Time']
+    mum = data['mum']
+    devstm = data['devstm']
+    mud = data['mud']
+    devstd = data['devstd']
+    Ncm = data['Ncm']
+    Ncd = data['Ncd']
+    hoP = data['hoP']
+    hoSc = data['hoSc']
+    hoC = data['hoC']
+    hwSc = data['hwSc']
+    hwP = data['hwP']
+    hwC = data['hwC']
+    N3 = data['N3']
+    num_steps3= data['num_steps3']
+    delta3 = data['delta3']
+    x = data['x']
+    Choose = data['Choose']
+
+  
+    return np.transpose(np.array(t)),np.array(mum).T,np.array(devstm).T,np.array(mud).T,np.array(devstd).T,Ncm,Ncd,hoP,hoSc,hoC,np.array(hwSc).T,np.array(hwP).T,np.array(hwC).T,N3,num_steps3,delta3,np.array(x).T,Choose
+
 
 #=======================================================================================
 #=======================================================================================
@@ -186,139 +269,52 @@ def Plot(gm,t,yP,yC,ySc,mud,devstd,samples3,Prob3):
 
 def main():
     
+    #Further investigations:
+    # diagonal covariance matrice neglet informations and indipedence of sampling
 
-#==========================================================
-#==Sampling Posterior Density probability(metropolis3)===== 
-#==========================================================
-
-#Further investigations:
-# diagonal covariance matrice neglet informations and indipedence of sampling
-
-#Time
-    t = np.array([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
-
-#Parameter of Forward model
-    hoP = 0.7
-    hoC = 0.8
-    hoSc = 3
-
-#Parameters of metropolis
-    N3 = 6
-    num_steps3 = 666
-    delta3 = 0.0011
-    x = np.array([0.01739949, 0.60231223, 0.00259897, 0.6211021,  0.00836138, 0.91836421])
-
-#[0.0188013,  0.5958105,  0.00245935, 0.59172465, 0.01059123, 0.90085271]) linear water 0.0011
-#[0.02,0.6,0.02,0.6,0.01,0.9]) #Starting point)
-#[0.01739949, 0.60231223, 0.00259897, 0.6211021,  0.00836138, 0.91836421]) #sin water 0.0011
-
-#Prior Model
-
-#Vsh CLAy,Rh clay, vsh snady clay, rh sandy clay, vox peat, rh peat
-    mum = np.array([0.02,0.6,0.02,0.6,0.01,0.9])
-    devstm= np.array([0.005,0.05,0.005,0.05,0.005,0.05])
-    #Sampling a normal distribution for calculate the covariance mantrix on prior parameteres
-    Ncm = 200 # number of samples to generate
-#generate N samples from the normal distribution
-    samplesm= np.zeros((Ncm,len(mum)))
-    for i in range(len(mum)):
-        samplesm[:,i] = np.random.normal(mum[i], devstm[i], Ncm)
-    covm = np.cov(samplesm.T)
+    #==========================================================
+    #============Estract data from JSON======================== 
+    #==========================================================
+    t,mum,devstm,mud,devstd,Ncm,Ncd,hoP,hoSc,hoC,hwSc,hwP,hwC,N3,num_steps3,delta3,x,Choose = ExtractData()
 
 
-#Data
-    s1 = 0.01 
-    s2 = 0.005 
-    s3 = 0.01 
-    s4 = 0.01 
-    s5 = 0.01 
-    s6 = 0.005 
-    s7 = 0.01 
-    s8 = 0.01 
-    s9 = 0.01 
-    s10 = 0.005
-    s11 = 0.01 
-    s12 = 0.01 
-    s13 = 0.005 
-    s14 = 0.01 
-    s15 = 0.005 
-    s16 = 0.01 
-    s17 = 0.01 
-    s18 = 0.005 
-    s19 = 0.01 
-    s20 = 0.005
-    s21 = 0.01 
-    mud = np.array([0,0.003,0.001,0.006,0.004,0.008,0.007,0.01,0.008,0.014,0.009,0.018,0.012,0.020,0.015,0.023,0.017,0.025,0.018,0.028,0.020])  
-    devstd=np.array([s1,s1,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21])  
- #Sampling a normal distribution for calculate the covariance mantrix on prior parameteres
-    Ncd = 200 # number of samples to generate
-# generate N samples from the normal distribution
-    samplesd= np.zeros((Ncd,len(mud)))
-    for i in range(len(mud)):    
-        samplesd[:,i] = np.random.normal(mud[i], devstd[i], Ncd)
-    covd = np.cov(samplesd.T)
-
-
+    #==========================================================
+    #================Groundwaterlevel========================== 
+    #==========================================================
+    yC,yP,ySc = Groundwaterlevel(t,hwSc,hwP,hwC,Choose,hoP,hoSc,hoC)
  
-#========= Groundwater level====================
 
-#Fixed value
-#hwP = 0.7
-#hwC = 0.8
-#hwSc = 3     
-   
-#Linear trend for hwet:
-#h wet Sandy clay
-    hwSc=np.array([2.5,1])
-    tfitSc=np.array([0,20])
-    B = np.stack([tfitSc, np.ones(len(tfitSc))]).T
-    mSc, cSc = np.linalg.lstsq(B, hwSc, rcond=None)[0]
-
-#h wet Peat
-    hwP=np.array([0.7,0.7])
-    tfitP=np.array([0,20]) 
-    C = np.stack([tfitP, np.ones(len(tfitP))]).T
-    mP, cP = np.linalg.lstsq(C, hwP, rcond=None)[0]
-
-#h wet clay
-    hwC=np.array([0.8,0.8])
-    tfitC=np.array([0,20])
-    A = np.stack([tfitC, np.ones(len(tfitC))]).T
-    mC, cC = np.linalg.lstsq(A, hwC, rcond=None)[0]
-
-    yC = mC*t + cC
-#ySc = mSc*t + cSc
-    yP = mP*t + cP
-
-#Sin trend for hwet:
-#h wet Sandy clay
-    ySc=mSc * t +cSc + 0.2 * np.sin(t-np.pi)
+    #==========================================================
+    #==============Covariance matrix========================== 
+    #==========================================================
+    covd,covm =CovarianceMatrix(mum,devstm,Ncm,Ncd,mud,devstd)
 
 
-#Calling metropolis3
-    samples3,Prob3 = metropolis3(N3, num_steps3, delta3, mum, covm, mud, covd, x,t, hoP, hoC, hoSc, yP, yC, ySc)
+    #==========================================================
+    #=================Metropolis=============================== 
+    #==========================================================
+    samples3,Prob3 = metropolis(N3, num_steps3, delta3, mum, covm, mud, covd, x,t, hoP, hoC, hoSc, yP, yC, ySc)
 
-
-
-#==========================================================
-#==============Only Forward model========================== 
-#==========================================================
+    #==========================================================
+    #==============Only Forward model========================== 
+    #==========================================================
     gm = ForwardModel(samples3,devstd,t,hoP,hoC,hoSc,yP, yC, ySc,mud)
 
 
-#====================================================
-#===========NewStartingPoint=========================
-#====================================================
+    #====================================================
+    #===========NewStartingPoint=========================
+    #====================================================
     NewStartingPoint = FindNewStartingPoint(Prob3,samples3)
 
-#====================================================
-#============Save in csv=============================
-#====================================================
+
+    #====================================================
+    #============Save in csv=============================
+    #====================================================
     SaveOutput(samples3, gm, Prob3)
 
-#====================================================
-#==================Plot==============================
-#====================================================
+    #====================================================
+    #==================Plot==============================
+    #====================================================
     Plot(gm,t,yP,yC,ySc,mud,devstd,samples3,Prob3)
 
 
